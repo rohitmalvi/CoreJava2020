c1894ee868259a65a3ff1f8f70645b3617d283b3
rohit malvi <rmalvi1995new1@gmail.com>
2020-11-30 13:05:56 +0530

    Update FebonakiSeries.java

--- FebonakiSeries.java
+++ FebonakiSeries.java
@@ -1,14 +1,18 @@
-package InterviewQuation;
+package com.example.demo;
 
-public class FebonakiSeries {
+public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		int first = 0;
-		for (int i = 1; i <= 100; i++) {
-			first = first + i;
-			if (first <= 100)
-				System.out.println(first + " ");
-		}
-	}
+		int num = 10;
+		int num1 = 0;
+		int num2 = 1;
 
+		for (int i = 1; i < num; i++) {
+			System.out.println(num1);
+			int sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+		}
+
+	}
 }
